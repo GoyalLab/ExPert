ac261ad4f4a2e72c1af74aa0bd676f9626dbbd05
Nitu Kumari <mwe1123@quser34.quest.it.northwestern.edu>
2024-10-15 12:59:59 -0500

    Ensure proper ordering of padded AnnData objects and fix bug in writing to h5ad

--- src/prepare.py
+++ src/prepare.py
@@ -12,24 +12,27 @@ def _filter(d, dataset_name, hvg_pool, zero_pad=True):
     # filter for pool of highly variable genes
     matching_hvgs = d.var_names.intersection(hvg_pool)
     logging.info(f'Found {len(matching_hvgs)} pool genes in {dataset_name}')
-    d = d[:, matching_hvgs]
+    d = d[:, matching_hvgs].copy()
+    # ensure appropriate typing
+    if not sp.isspmatrix_csr(d.X):
+        logging.debug(f'Changing .X from {d.X.__class__} to scipy.sparse._csr.csr_matrix')
+        d.X = sp.csr_matrix(d.X, dtype=np.float32)
     # check for missing genes
     missing_hvgs = set(hvg_pool) - set(matching_hvgs)
     if zero_pad and len(missing_hvgs) > 0:
         logging.info(f'{len(missing_hvgs)} hvgs missing from pool; padding with zero values')
         # build empty matrix for padded matrix
-        zero_matrix = sp.csc_matrix((d.n_obs, len(missing_hvgs)), dtype=np.float32)
+        zero_matrix = sp.csr_matrix((d.n_obs, len(missing_hvgs)), dtype=np.float32)
         # stack matrices horizontally
         logging.debug(f'Adding sparse 0-pad to .X ({d.n_obs}, {len(missing_hvgs)})')
         X = sp.hstack((d.X, zero_matrix))
         # define proper gene assignments, missing genes is unordered, but does not matter (all are 0)
-        gene_list = matching_hvgs + missing_hvgs
+        gene_list = list(matching_hvgs) + (list(missing_hvgs))
         # build final AnnData object
         logging.debug('Updating padded AnnData')
-        d = sc.AnnData(X=X, obs=d.obs, var=pd.DataFrame(index=list(gene_list)))
-        logging.debug('Ordering genes in padded AnnData')
-        # ensure .var is ordered in the correct way after merge
-        d = d[:, hvg_pool]
+        d = sc.AnnData(X=X, obs=d.obs, var=pd.DataFrame(index=gene_list))
+        logging.debug('Ordering genes based on gene pool')
+        d = d[:, hvg_pool].copy()
     else:
         logging.info(f'{len(missing_hvgs)} hvgs lost from pool')
     return d
